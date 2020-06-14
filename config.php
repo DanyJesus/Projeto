4e9370b55020b77c886d1a5825d3d3096577a027
DanyJesus <56988997+DanyJesus@users.noreply.github.com>
2020-06-14 11:54:24 -0300

    alterar  root

--- config.php
+++ config.php
@@ -2,7 +2,8 @@
 
 session_start(); //Inicia uma nova sessão ou resume uma sessão existente
 //dados de acesso ao banco
-$local = "34.95.220.141";
+//$local = "34.95.220.141";
+$local = "34.95.177.219";
 $usuario_BD = "root";
 $senha_BD = "1vdwsGhkOhkmhnAx";
 $base = "bprojeto";
