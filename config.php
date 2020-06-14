6c4dfbccd3f7aa3f3660bdcc66406193de6d3a00
DanyJesus <56988997+DanyJesus@users.noreply.github.com>
2020-06-14 12:45:29 -0300

    Update config.php

--- config.php
+++ config.php
@@ -2,8 +2,8 @@
 
 session_start(); //Inicia uma nova sessão ou resume uma sessão existente
 //dados de acesso ao banco
-//$local = "34.95.220.141";
-$local = "34.95.177.219";
+$local = "34.95.220.141";
+
 $usuario_BD = "root";
 $senha_BD = "1vdwsGhkOhkmhnAx";
 $base = "bprojeto";
